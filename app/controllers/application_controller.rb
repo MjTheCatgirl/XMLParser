920e15d5d60fb3d499ba7a1cbd4ba325d6531014
Amanda Himmerich <amanda.himmerich@gmail.com>
2018-10-05 13:47:41 -0500

    displays right in browser too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 	class Property
     # This is used when creating a new property object with the new keyword. You must provide the
     # constructor with a property_id, name, and email upon creation.
-    def initialize(property_id, name, email)
+    def initialize(property_id, property_name, email)
       @property_id = property_id
-      @name = name
+      @name = property_name
       @email = email
     end
     # Allows us to access the property_id attribute with dot notation in view
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
    	@property_id
    end
    # Allows us to access the name attribute with dot notation in view
-   def name
+   def property_name
    	@name
    end
    # Allows us to access the email attribute with dot notation in view
@@ -37,8 +37,7 @@ class ApplicationController < ActionController::Base
 	# is added to an array of property objects, @propertiesArray. This array can then be used by our
 	# view to iterate through the array of properties and display each attribute of each one. 
 	# 
-	# This works as expected for the provided XML file, just need to use view to display the info
-	# properly
+	# This works as expected for the provided XML file and displays results in browser
 	def scrape_properties
 		# Pull in the page to parse
 		require 'open-uri'
@@ -57,7 +56,7 @@ class ApplicationController < ActionController::Base
   		@properties.each do |node|
   			# These three do not throw errors now
   			newID = node.at_xpath(".//Identification/@IDValue").value
-  			newName = node.xpath(".//MarketingName").text
+  			newName = node.at_xpath(".//MarketingName").text
   			newEmail = node.xpath(".//Email").text
   			# Make a new Property object using these values, no errors now
   			@newProperty = Property.new(newID, newName, newEmail)
